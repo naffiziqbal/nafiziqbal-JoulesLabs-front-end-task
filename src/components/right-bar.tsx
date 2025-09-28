66ee9897770a481db2d0ac88b66ef0e62b850f0e
naffiziqbal <f.nafiziqbal@gmail.com>
2025-09-28 21:30:20 +0600

    Refactor App component to use new layout with SideBar, MainScreen, and RightBar; removed ReactFlow and related state management.

---
+++ src/components/right-bar.tsx
@@ -0,0 +1,10 @@
+export default function RightBar() {
+  return (
+    <div className="w-full bg-white pt-3">
+      <h4 className="font-bold text-lg"> Properties</h4>
+      <p className="text-gray-500 text-sm">
+        Select a node to see read-only properties.
+      </p>
+    </div>
+  );
+}
