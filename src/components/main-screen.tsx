0ee58e05ef99831922613787e38af23dd02e6f87
naffiziqbal <f.nafiziqbal@gmail.com>
2025-09-28 21:55:45 +0600

    Update MainScreen to support dynamic node creation using UUID; add uuid package to dependencies.

--- src/components/main-screen.tsx
+++ src/components/main-screen.tsx
@@ -1,42 +1,83 @@
-import { useState, useCallback } from "react";
 import {
-  ReactFlow,
-  applyNodeChanges,
-  applyEdgeChanges,
   addEdge,
-  Controls,
   Background,
   BackgroundVariant,
+  Controls,
   MiniMap,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+  type Connection,
+  type Edge,
+  type Node,
+  type ReactFlowInstance,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-const initialNodes = [
-  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
-  { id: "n2", position: { x: 0, y: 100 }, data: { label: "Node 2" } },
-];
-const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
+import { useCallback, useRef, useState } from "react";
+import { NODE_TEMPLATES } from "../data/data";
+import { v4 as uuidv4 } from "uuid";
 
 export default function MainScreen() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState([] as Node[]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([] as Edge[]);
+  const [rfInstance, setRfInstance] = useState<ReactFlowInstance | null>(null);
 
-  const onNodesChange = useCallback(
-    (changes) =>
-      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-    []
+  const onDragOver = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy";
+  }, []);
+
+  const onDrop = useCallback(
+    (event: React.DragEvent) => {
+      event.preventDefault();
+      if (!wrapperRef.current) return;
+      console.log(nodes);
+      const type = event.dataTransfer.getData("application/reactflow");
+      if (!type) return;
+      const template = NODE_TEMPLATES.find((t) => t.type === type);
+      const reactFlowBounds = wrapperRef.current.getBoundingClientRect();
+
+      const position = rfInstance
+        ? rfInstance.project({
+            x: event.clientX - reactFlowBounds.left,
+            y: event.clientY - reactFlowBounds.top,
+          })
+        : {
+            x: event.clientX - reactFlowBounds.left,
+            y: event.clientY - reactFlowBounds.top,
+          };
+
+      const id = uuidv4();
+      const newNode: Node = {
+        id,
+        position,
+        data: {
+          label: template?.label || type,
+          userData: { ...template?.defaultData, type },
+        },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [rfInstance, setNodes]
   );
-  const onEdgesChange = useCallback(
-    (changes) =>
-      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-    []
-  );
+
   const onConnect = useCallback(
-    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
-    []
+    (connection: Connection) => {
+      const edge = addEdge(connection as Edge, edges);
+      setEdges(edge as Edge[]);
+    },
+    [edges, setEdges]
   );
 
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div
+      style={{ width: "100%", height: "100vh" }}
+      ref={wrapperRef}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+    >
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -47,8 +88,7 @@ export default function MainScreen() {
       >
         <Background variant={BackgroundVariant.Dots} bgColor="#212121" />
         <Controls />
-        <MiniMap />
-    
+        {/* <MiniMap /> */}
       </ReactFlow>
     </div>
   );
